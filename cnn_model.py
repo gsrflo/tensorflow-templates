7a8cb6bd210aea3e9ea0091a32a9509214cfe50b
gsrflo <fgeiser@users.noreply.github.com>
2020-10-26 11:14:58 +0100

    preprocessing data

--- cnn_model.py
+++ cnn_model.py
@@ -16,3 +16,72 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 #matplotlib inline
+
+# Uncomment if GPU available:
+# physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# print("Num GPUs Available: ", len(physical_devices))
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# Get working directory and change to data folder
+dirname = os.path.dirname(__file__)
+dirdata = os.path.join(dirname, 'dogs-vs-cats')
+os.chdir(dirdata)
+
+# Create sub-folders
+if os.path.isdir('train/dog') is False:
+    os.makedirs('train/dog')
+    os.makedirs('train/cat')
+    os.makedirs('valid/dog')
+    os.makedirs('valid/cat')
+    os.makedirs('test/dog')
+    os.makedirs('test/cat')
+
+    # Do not use whole dataset but get a subset
+    for c in random.sample(glob.glob('dog*'), 500):
+        shutil.move(c, 'train/dog')
+    for c in random.sample(glob.glob('cat*'), 500):
+        shutil.move(c, 'train/cat')
+    for c in random.sample(glob.glob('dog*'), 100):
+        shutil.move(c, 'valid/dog')
+    for c in random.sample(glob.glob('cat*'), 100):
+        shutil.move(c, 'valid/cat')
+    for c in random.sample(glob.glob('dog*'), 50):
+        shutil.move(c, 'test/dog')
+    for c in random.sample(glob.glob('cat*'), 50):
+        shutil.move(c, 'test/cat')
+os.chdir('../../')
+
+train_path = os.path.join(dirname, 'dogs-vs-cats/train')
+valid_path = os.path.join(dirname, 'dogs-vs-cats/valid')
+test_path = os.path.join(dirname, 'dogs-vs-cats/test')
+
+# Preprocessing: Generating batches of tensor image data
+train_batches =  ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=train_path, target_size=(224,224), classes=['dog', 'cat'], batch_size=10)
+
+valid_batches =  ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=valid_path, target_size=(224,224), classes=['dog', 'cat'], batch_size=10)
+
+test_batches =  ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=test_path, target_size=(224,224), classes=['dog', 'cat'], batch_size=10, shuffle=False)
+
+assert train_batches.n == 1000
+assert valid_batches.n == 200
+assert test_batches.n == 100
+assert train_batches.num_classes == valid_batches.num_classes == test_batches.num_classes == 2
+
+imgs, labels = next(train_batches)
+
+# This function will plot images in the form of a grid with 1 row and 10 columns
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 10, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+# Show a batch of training data
+plotImages(imgs)
+print(labels)
